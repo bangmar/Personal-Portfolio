dcb830a246dda2d08ec02e89e062f4d87a4d044c
MARIO SIHALOHO <20102018@ittelkom-pwt.ac.id>
2023-08-07 10:16:31 +0700

    feature : project init

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+	return (
+		<div className='w-full bg-neutral-200 grid place-items-center h-screen'>
+			<h1 className='font-bold'>Setup Complete</h1>
+		</div>
+	);
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const browserRouter = createBrowserRouter([
+	{
+		path: "/",
+		element: <App />,
+	},
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+	<React.StrictMode>
+		<RouterProvider router={browserRouter} />
+	</React.StrictMode>
+);
